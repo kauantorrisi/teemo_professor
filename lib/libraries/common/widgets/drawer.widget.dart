39c707911153c9a67fbca8d15866a14c59b7fc36
Kauan Torrisi <torrisikauan@gmail.com>
2022-11-27 15:04:58 -0300

    removed drawer button to navigate to top ranked page

--- lib/libraries/common/widgets/drawer.widget.dart
+++ lib/libraries/common/widgets/drawer.widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:teemo_professor/libraries/common/design/colors.dart';
 import 'package:teemo_professor/libraries/common/design/texts.dart';
@@ -11,52 +12,42 @@ class DrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-
-    return Drawer(
-      backgroundColor: TPColor.white,
-      width: 250,
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          vertical: screenSize.height * 0.04,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Olá,\nInvocador', style: TPTexts.t1()),
-            const Divider(color: TPColor.darkBlue),
-            ListTileWidget(
-                label: 'Pesquisar invocador',
-                icon: Icons.search_rounded,
-                onTap: () {
-                  Modular.to.navigate(Modular.initialRoute);
-                  Modular.to.pop();
-                }),
-            const SizedBox(height: 5),
-            ListTileWidget(
-                label: 'Campeões',
-                icon: Icons.theater_comedy,
-                onTap: () {
-                  Modular.to.navigate('/champions-module/');
-                  Modular.to.pop();
-                }),
-            const SizedBox(height: 5),
-            ListTileWidget(
-                label: 'Itens',
-                icon: Icons.settings,
-                onTap: () {
-                  Modular.to.navigate('/itens-module/');
-                }),
-            const SizedBox(height: 5),
-            ListTileWidget(
-              label: 'Top Ranked',
-              icon: Icons.scoreboard_rounded,
-              onTap: () {
-                Modular.to.navigate('/scoreboard-module/');
-              },
-            )
-          ],
+    return SafeArea(
+      child: Drawer(
+        backgroundColor: TPColor.white,
+        width: 250.w,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 0.4.h),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Olá,\nInvocador', style: TPTexts.t1()),
+              const Divider(color: TPColor.darkBlue),
+              ListTileWidget(
+                  label: 'Pesquisar invocador',
+                  icon: Icons.search_rounded,
+                  onTap: () {
+                    Modular.to.navigate(Modular.initialRoute);
+                    Modular.to.pop();
+                  }),
+              SizedBox(height: 5.h),
+              ListTileWidget(
+                  label: 'Campeões',
+                  icon: Icons.theater_comedy,
+                  onTap: () {
+                    Modular.to.navigate('/champions-module/');
+                    Modular.to.pop();
+                  }),
+              SizedBox(height: 5.h),
+              ListTileWidget(
+                  label: 'Itens',
+                  icon: Icons.settings,
+                  onTap: () {
+                    Modular.to.navigate('/itens-module/');
+                  }),
+            ],
+          ),
         ),
       ),
     );
